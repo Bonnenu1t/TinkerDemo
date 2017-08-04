7c2b1b4a09f53c0ed991305891a63c1102ac9c53
Bonnenu1t <493632931@qq.com>
2017-08-04 20:59:31 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/tinkerdemo/IApplication.java
@@ -0,0 +1,16 @@
+package com.bw.tinkerdemo;
+
+import android.app.Application;
+
+/**
+ * Created by Bonnenu1t丶 on 2017/8/2.
+ */
+
+public class IApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
